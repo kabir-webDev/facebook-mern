435906f8f94cfa8d77cb94945efd38bf96e14da3
kabir-webDev <kabir.webdev@gmail.com>
2020-10-07 09:14:45 +0600

    Firebase Auth Added

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,20 @@
-import firebase from 'firebase'
+import firebase from "firebase";
 
 const firebaseConfig = {
-    // this is where your firebase config goes
+  apiKey: "AIzaSyAUuxzTI_pqwgEBfcyWw0Cx8rxSe6dgcF8",
+  authDomain: "facebook-mern-333.firebaseapp.com",
+  databaseURL: "https://facebook-mern-333.firebaseio.com",
+  projectId: "facebook-mern-333",
+  storageBucket: "facebook-mern-333.appspot.com",
+  messagingSenderId: "835774374889",
+  appId: "1:835774374889:web:75f84396636b730bfc6178",
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig)
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth()
-const provider = new firebase.auth.GoogleAuthProvider()
-const db = firebase.firestore()
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const db = firebase.firestore();
 
-export { auth, provider }
-export default db
+export { auth, provider };
+export default db;
